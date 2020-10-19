2ae401f369b7fa7cef15c92515543e5978753023
mamulini <mamulini@yahoo.com>
2020-10-19 20:42:53 +0300

    added basname attr to BrouserRouter for gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename={'/my-portfolio'}>
     <App />
   </BrowserRouter>,
   // </React.StrictMode>,
